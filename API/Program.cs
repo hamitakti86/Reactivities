4893b02524a187b3d7acc9c3904a486b296904ba
ELA <ela@Macintosh-MBP.home>
2022-03-25 22:52:08 +0300

    added user account

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -17,20 +19,21 @@ namespace API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using var scope =host.Services.CreateScope();
+            using var scope = host.Services.CreateScope();
 
             var services = scope.ServiceProvider;
 
             try
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex,"An error occured during migration ");
+                logger.LogError(ex, "An error occured during migration ");
             }
 
             await host.RunAsync();
